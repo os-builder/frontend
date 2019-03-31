9bfdfb5fff64e5dd99adc270c22a07ab2a14efab
b4rti <b.gorol@efero.de>
2019-03-31 20:03:33 +0200

    removing flask

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,9 @@
-FROM python:alpine
-
-RUN apk add npm
+FROM ####
 
 WORKDIR /app
 
-COPY ./requirements.txt /app/requirements.txt
-
-RUN pip install -r requirements.txt
-
-COPY ./package.json /app/package.json
+COPY ./package.json .
 
 RUN npm install
 
-COPY ./ /app
-
-run npm run build
-
-EXPOSE 7000
-
-CMD ["python", "manage.py", "runserver"]
+COPY . .
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '@/views/Home.vue'
 
 Vue.use(Router)
 
@@ -16,7 +16,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      component: () => import('./views/About.vue')
+      component: () => import('@/views/About.vue')
     }
   ]
 })
